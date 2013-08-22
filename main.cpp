6cd2814e653941c59f21c7fe1c83f9afa9d74ecf
Alexander <gakarak@gmail.com>
2013-08-22 18:24:03 +0300

    Update main.cpp
    removed comments with unused code.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
-//#include <QCoreApplication>
 #include <QApplication>
-
-/*
-#include <iostream>
-#include <vector>
-#include <cv.h>
-#include <highgui.h>
-*/
-
 
 using namespace std;
 
@@ -19,20 +10,4 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     return app.exec();
-
-/*
-    vector<cv::Point>   data;
-    vector<int>         labels;
-    for(int ii=0; ii<8; ii++) {
-        data.push_back(cv::Point(ii,ii));
-        labels.push_back(ii);
-    }
-    cv::Mat retData, retLabels;
-    cv::Mat(data).copyTo(retData);
-    retData = retData.reshape(1, retData.rows);
-    retData.convertTo(retData, CV_32FC1);
-    cv::Mat(labels).copyTo(retLabels);
-    cout << "(" << retData.cols   << "x" << retData.rows << ":" << retData.channels()  << ") = " << retData   << endl;
-    cout << "(" << retLabels.cols << "x" << retLabels.rows << ":" << retLabels.channels() << ") = " << retLabels << endl;
-*/
 }
